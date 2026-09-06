055872c69e1ebddc407ae630fbee1b00700ce7f3
MahboobGeeky <mahboob9184@gmail.com>
2026-09-06 23:34:14 +0530

    Done

--- 10. Reverse an Array 87/3-Solution.cpp
+++ 10. Reverse an Array 87/3-Solution.cpp
@@ -1,10 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     cin>>n;
-
     int arr[n];
     for(int i=0; i<n; i++) cin>>arr[i];
 
@@ -13,6 +11,5 @@ int main() {
         cout<<arr[i];
     }
     cout<<endl;
-
     return 0;
 }
